7c3813d6886db17fecc2fe32abcc424c949eb2cc
Aq0amancer <predrag364@gmail.com>
2021-04-27 14:26:07 +0200

    null

--- Heatmort/IO_functions.py
+++ Heatmort/IO_functions.py
@@ -120,7 +120,7 @@ def meanSummerTemperatures(relative_path,date,rcp_scenario, ssp_scenario, tas_pe
     climate_data=loadClimate(relative_path,date,rcp_scenario, ssp_scenario, tas_percentil,uhi) # Load data first
     year=int(date[0:4]) #get first year from the date string
     mean_summer_temperatures_all_years=np.empty((90,134))
-    for year_value in range(year,year+1):
+    for year_value in range(year,year+5):
         mean_summer_temperatures_year=np.mean(climate_data['daily_climrisk_tas'].sel(time=slice(str(year_value)+'-4', str(year_value)+'-9')), axis=0)
         mean_summer_temperatures_all_years=np.stack((mean_summer_temperatures_all_years,mean_summer_temperatures_year))  
         print(mean_summer_temperatures_all_years.shape)
