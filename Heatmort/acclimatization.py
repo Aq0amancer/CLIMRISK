4edcaa854252f98991267b77d330af8afaee687a
Aq0amancer <predrag364@gmail.com>
2021-04-29 12:39:09 +0200

    null

--- Heatmort/acclimatization.py
+++ Heatmort/acclimatization.py
@@ -39,7 +39,8 @@ def acclimatization(rcp_scenario, ssp_scenario, tas_percentil, uhi):
                 mean_summer_temperatures_date_diff=np.zeros((90,134))
             else:
                 mean_summer_temperatures_date_diff=np.dstack((mean_summer_temperatures_date_diff,np.squeeze(mean_summer_temperatures_date[year_index,:,:]-mean_summer_temperatures_date[year_index-1,:,:])))
-    print(mean_summer_temperatures_date_diff.shape)
+    # Shuffle dimensions around to fit NC standard (time,lat,lon)
+    mean_summer_temperatures_date_diff=np.transpose(mean_summer_temperatures_date_diff, (2, 0, 1))
     template_file = path + '/Masks/year_template/year_template.nc'
     with xr.open_dataset(template_file) as yearly_mask:
     #Convert adjr2 and rmse data to xarray.Dataset format
